a774df552b53e2eb980e810039ec1743e9ba8147
kesfc <kevinli20030305@gmail.com>
2023-11-29 18:57:17 -0500

    added Lat and Lon into result for optics model
    added Lat and Lon into result for optics model

--- optics.py
+++ optics.py
@@ -10,11 +10,11 @@ with open(file, newline='') as file:
     for row in reader:
         data.append(row)
 file = ("data\Child_Care_Centers_clean.csv")
-locations = []
+locations = dict()
 with open(file, newline='') as file:
     reader = csv.reader(file)
     for row in reader:
-        locations.append(row)
+        locations[row[0]] = [row[1], row[2]]
 train_data = data[1:][1:]
 # Convert the list of lists to float values for clustering
 data_float = [[float(val) for val in row] for row in train_data]
@@ -33,14 +33,17 @@ labels = clustering.labels_
 # Print the cluster labels
 a = set(labels)
 print(a)
-file_path = 'optics_cluster.csv'
+file_path = 'data\optics_result.csv'
 # Writing data to CSV file
 with open(file_path, 'w', newline='') as csvfile:
     # creating a csv writer object
     csvwriter = csv.writer(csvfile)
     # writing the fields
     
-    csvwriter.writerow(['ID', 'Label'])
+    csvwriter.writerow(['ID', 'Latitude', 'Longitude', 'Label'])
     # writing the data rows
     for i in range(0, len(labels)):
-        csvwriter.writerow([data[i+1][0], labels[i]])
+        ID = str(round(float(data[i+1][0])))
+        if len(ID) != len("1222293233"):
+            ID = '0'*(len("1222293233")-len(ID)) + ID
+        csvwriter.writerow([ID, locations[ID][0], locations[ID][1], labels[i]])
